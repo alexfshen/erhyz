d759be9910a6d005c91e27c3b5d7e63c40f34ced
fang.shen <fang.shen@le-campus-numerique.fr>
2024-07-17 12:05:47 +0200

    added ollama but doesn't work

--- gallery_dynamic.py
+++ gallery_dynamic.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template
+import ollama
 
 #################
 
@@ -7,15 +8,43 @@ directory_path = '/home/fang.shen@Digital-Grenoble.local/Documents/Dev Web/ollam
 files = os.listdir(directory_path)
 
 
-################
+###########################################
+
+
+############################################
+def generate_stories(images) :
+    stories = []
+    for image in images :
+        image_path = os.path.join(directory_path, image)
+        response = ollama.chat(
+            model = "llava",
+            messages = [
+                {
+                    'role' : 'user',
+                    'content' : 'Create a story based on this image :',
+                    'imagines' : [image_path]
+                }
+            ]
+        )
+        story = response['message']['content']
+        stories.append(story)
+    return stories
+
+##########################################
 
 app = Flask(__name__)
 
 @app.route('/')
 def gallery() :
-    my_images = files
-    my_image = '1.jpg'
-    return render_template('gallery.html')
+    try : 
+        image_stories = generate_stories(files)
+        data = [{'image' : image, 'story' : story} for story in zip(files, image_stories)]
+        return render_template('gallery2.html', data = data)
+    except Exception as e:
+        print(f"Error in gallery route: {str(e)}")
+        return "An error occurred while processing images and stories."
 
 if __name__ == '__main__':
     app.run(debug=True, port = 5001)
+
+  
